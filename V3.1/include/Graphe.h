d3ca19f6c4dbc9158713686e089a105ceee399da
paulmrq <49637316+paulmrq@users.noreply.github.com>
2019-04-17 16:09:01 +0200

    debut kruskal

---
+++ V3.1/include/Graphe.h
@@ -0,0 +1,39 @@
+#ifndef GRAPHE_H
+#define GRAPHE_H
+
+#include <vector>
+#include <fstream>
+#include <iostream>
+#include <allegro.h>
+#include "Sommet.h"
+#include <string>
+#include <sstream>
+#include <algorithm>
+
+class Graphe//:public Sommet
+{
+    public:
+        Graphe();
+        virtual ~Graphe();
+        void remplissage(BITMAP *fond) const;
+        void remplissage_arrete(BITMAP *fond) const;
+        void remplissage_poids(BITMAP *fond) const;
+        void affiche_vect();
+        void affiche_tab();
+        void affiche_poids();
+        void Kruskal();
+        int getOrdre();
+
+    protected:
+
+    private:
+        std::vector<Sommet> m_Sommets;
+        int m_ordre,m_taille;
+        Sommet *m_voisins_som[2][sizeof(m_Sommets)]; //new sommet... pr completer
+        //int m_voisins_id[3][sizeof(m_Sommets)];
+        //std::vector <std::vector<int>> m_voisins_id; //vect a 2 dim mais c mieux de lineariser
+        std::vector <int>  m_voisins_id;
+        std::vector <float> m_poids;
+};
+
+#endif // GRAPHE_H
