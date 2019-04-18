97acc2815dea4c26ea377cc48ed55f7f4fd47363
Solene8 <49645293+Solene8@users.noreply.github.com>
2019-04-18 19:02:06 +0200

    Add v3.3

---
+++ V3.2/src/Arrete.cpp
@@ -0,0 +1,43 @@
+#include "Arrete.h"
+
+Arrete::Arrete(Sommet S1, Sommet S2, float p1, float p2, int id)
+{
+   m_S1=S1;
+   m_S2=S2;
+   m_p1=p1;
+   m_p2=p2;
+   m_id=id;
+
+}
+
+Arrete::~Arrete()
+{
+    //dtor
+}
+
+void Arrete::setS1(Sommet S1)
+{
+    m_S1=S1;
+}
+void Arrete::setS2(Sommet S2)
+{
+    m_S2=S2;
+}
+void Arrete::setp1(float p1)
+{
+    m_p1=p1;
+}
+void Arrete::setp2(float p2)
+{
+    m_p2=p2;
+}
+void Arrete::affiche()
+{
+   // std::cout<<"YOLO"<<std::endl;
+    std::cout<<"id : "<<m_id<<std::endl;
+    std::cout<<"sommet1 : "<<std::endl;
+    m_S1.affiche();
+    std::cout<<"sommet2 : "<<std::endl;
+    m_S2.affiche();
+    std::cout<<"poids 1 et 2 "<<m_p1<<" ; "<<m_p2<<std::endl;
+}
