46da449992e1cd0b75c7052574e4dd10b9249bdb
paulmrq <49637316+paulmrq@users.noreply.github.com>
2019-05-05 14:14:41 +0200

    Add files via upload

---
+++ Dijkstra/main.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include "Sommet.h"
+#include <vector>
+#include <fstream>
+#include "Graphe.h"
+#include <allegro.h>
+
+using namespace std;
+
+#define HEIGHT 480
+#define WIDTH 640
+
+
+void ini_alleg()///routine d'initialisation allegro
+{
+    allegro_init();
+    set_display_switch_mode(SWITCH_BACKGROUND);
+    install_keyboard();
+    install_mouse();
+
+
+    set_color_depth(desktop_color_depth());
+    if (set_gfx_mode(GFX_AUTODETECT_WINDOWED,WIDTH,HEIGHT,0,0)!=0)
+    {
+        allegro_message("prb gfx mode");
+        allegro_exit();
+        exit(EXIT_FAILURE);
+    }
+
+
+}
+
+int main()
+{
+    ini_alleg();
+    Graphe g;
+
+    BITMAP* fond;
+    fond=create_bitmap(WIDTH,HEIGHT);
+    int quit=0;
+    cout<<"test"<<endl;
+    while(quit!=4)
+    {
+        //menu
+        cout<<"1 pr allegro 4 quit"<<endl;
+        cin>>quit;
+        switch(quit)
+        {
+        case 1:
+
+            g.affiche_vect();
+            cout<<"/////////////////////////"<<endl;
+            g.affiche_tab();
+            cout<<"/////////////////////////"<<endl;
+            g.affiche_poids();
+            cout<<"/////////////////////////"<<endl;
+            cout<<"/////////vect"<<endl;
+            g.remplissage_vectArrete();
+            for(int i=0; i < g.getVectArrete().size(); i++) {
+                    cout<<"HELLO"<<endl;
+                g.getVectArrete()[i].affiche();
+            }
+            cout<<"KRUSKAL-------"<<endl;
+            g.kruskal();
+            cout<<""<<endl;
+            cout<<"DIJKSTRA-------"<<endl;
+            g.dijkstra();
+
+            while (!key[KEY_ESC])
+            {
+
+                clear_bitmap(fond);
+                g.remplissage(fond);
+                g.remplissage_arrete(fond);
+                g.remplissage_poids(fond);
+
+
+                blit(fond,screen,0,0,0,0,WIDTH,HEIGHT);
+            }
+
+            break;
+
+        case 4:
+            return 0;
+        }
+
+
+    }
+    return 0;
+}
+END_OF_MAIN();
---
+++ Dijkstra/src/Arrete.cpp
@@ -0,0 +1,61 @@
+#include "Arrete.h"
+
+Arrete::Arrete(Sommet S1, Sommet S2, float p1, float p2, int id)
+{
+   m_S1=S1;
+   m_S2=S2;
+   m_p1=p1;
+   m_p2=p2;
+   m_id=id;
+
+}
+
+Arrete::~Arrete()
+{
+    //dtor
+}
+
+float Arrete::getp1()
+{
+    return m_p1;
+}
+float Arrete::getp2()
+{
+    return m_p2;
+}
+int Arrete::getidS1()
+{
+    return m_S1.getID();
+}
+int Arrete::getidS2()
+{
+    return m_S2.getID();
+}
+
+
+void Arrete::setS1(Sommet S1)
+{
+    m_S1=S1;
+}
+void Arrete::setS2(Sommet S2)
+{
+    m_S2=S2;
+}
+void Arrete::setp1(float p1)
+{
+    m_p1=p1;
+}
+void Arrete::setp2(float p2)
+{
+    m_p2=p2;
+}
+void Arrete::affiche()
+{
+   // std::cout<<"YOLO"<<std::endl;
+    std::cout<<"id : "<<m_id<<std::endl;
+    std::cout<<"sommet1 : "<<std::endl;
+    m_S1.affiche();
+    std::cout<<"sommet2 : "<<std::endl;
+    m_S2.affiche();
+    std::cout<<"poids 1 et 2 "<<m_p1<<" ; "<<m_p2<<std::endl;
+}
